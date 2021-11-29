9372ed1a30ccc483f2718df4c331a376aa1aa48f
Hanan <hanan@rebase.ventures>
2021-11-29 10:34:22 +0200

    Fix typecheck

--- src/dev/index.tsx
+++ src/dev/index.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import UseCsvButton from '../index';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import UseCsvButton from "../index";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div style={{
-      position: "absolute",
-      top: "50%",
-      left: "50%",
-      transform: "translateX(-50%) translateY(-50%)"
-    }}>
-      <UseCsvButton />
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translateX(-50%) translateY(-50%)",
+      }}
+    >
+      <UseCsvButton importerKey="your-importer-key" user={{ userId: "12345" }} metadata={{ anotherId: "12345" }}>
+        Import Data
+      </UseCsvButton>
     </div>
-  </React.StrictMode >,
-  document.getElementById('root')
+  </React.StrictMode>,
+  document.getElementById("root"),
 );
